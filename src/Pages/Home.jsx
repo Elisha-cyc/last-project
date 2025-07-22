10a8b42beb34e4175b91dc2ab78e0228066d7e7b
Elisha-cyc <elishaocansey23@gmail.com>
2025-07-22 13:04:12 +0000

    first commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Hero from '../Components/Hero'
+import FeaturedDestination from '../Components/FeaturedDestination'
+import ExclusiveOffers from '../Components/ExclusiveOffers'
+import Testimonial from '../Components/Testimonial'
+import NewsLetter from '../Components/NewsLetter'
+
+export const Home = () => {
+  return (
+    <>
+        <Hero />
+        <FeaturedDestination />
+        <ExclusiveOffers/>
+        <Testimonial/>
+        <NewsLetter/>
+    </>
+  )
+}
+export default Home
