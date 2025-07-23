d93fbfd2cd1a5ff6d577fba44091a05604961987
Elisha-cyc <elishaocansey23@gmail.com>
2025-07-23 21:29:09 +0000

    h1

--- src/App.jsx
+++ src/App.jsx
@@ -29,8 +29,8 @@ export const App = () => {
           <Route path="/rooms/:id" element={<RoomDetails />} />
           <Route path="/my-bookings" element={<MyBookings />} />
           <Route path="/owner" element={<LayOut />}></Route>
-          <Route path='/owner/dashboard' element= {<DashBoard />}/>
-          <Route path='/owner/add-room' element= {<AddRoom/>}/>
+          <Route  element= {<DashBoard />}/>
+          <Route path='add-room' element= {<AddRoom/>}/>
           <Route path='list-room' element= {< ListRoom/>}/>
         </Routes>
       </div>
