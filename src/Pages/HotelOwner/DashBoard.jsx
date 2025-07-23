d998fd018b6c85b8dfbd39b19ee61be9e684096c
Elisha-cyc <elishaocansey23@gmail.com>
2025-07-23 22:01:36 +0000

    DashBoard.jsx

--- src/Pages/HotelOwner/DashBoard.jsx
+++ src/Pages/HotelOwner/DashBoard.jsx
@@ -9,7 +9,7 @@ const DashBoard = () => {
     <div>
         <Title align='left' font='outfit' title='Dashboard'subTitle='Monitor your room listings, track booking and analyze revenue-all in one place. Stay updated with real-time insights to ensure smooth operations' />
         <div className="flex gap-4 my-8">
-            <div className="bg-primary/3 border border-primary/10 rounded flex p-4 pr-8">
+            <div className="bg-primary/3 border border-primary/10 rounded flex mb-10 pr-8">
                 <img src={assets.totalBookingIcon} alt="" className='max-sm:hidden h-10'/>
                 <div className="flex flex-col sm:ml-4 font-medium">
                     <p className='text-blue-500 text-lg'>Total Bookings</p>
